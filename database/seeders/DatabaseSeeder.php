05a43e7adc773066b1157ec306f69a71630f457d
Bado239 <sallbado106@gmail.com>
2026-07-03 11:38:24 +0000

    Fix: Mise à jour des catégories et projets par défaut dans le Seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,12 +12,14 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // Crée des catégories de test
-        Category::firstOrCreate(['name' => 'Professionnel']);
-        Category::firstOrCreate(['name' => 'Personnel']);
-        Category::firstOrCreate(['name' => 'Études']);
+        Category::firstOrCreate(['name' => 'Master ISEF1']);
+        Category::firstOrCreate(['name' => 'CGP']);
 
         // Crée des projets de test
-        Project::firstOrCreate(['title' => 'Développement Web']);
-        Project::firstOrCreate(['title' => 'Organisation']);
+        Project::firstOrCreate(['title' => 'VARQUAL']);
+        Project::firstOrCreate(['title' => 'Calcul Stochastique']);
+        Project::firstOrCreate(['title' => 'Finance']);
+        Project::firstOrCreate(['title' => 'Etude 3FPT']);
+        Project::firstOrCreate(['title' => 'Pauvreté']);
     }
 }
