4554c4a3b334a1258b34165f4c264cc0f3283ed3
Bado239 <sallbado106@gmail.com>
2026-08-05 10:45:55 +0000

    Fix: Correction du nom de route mode.switch

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,12 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', [TaskController::class, 'index'])->name('dashboard');
-Route::get('/switch-mode/{mode}', [TaskController::class, 'switchMode'])->name('switch.mode');
+
+// Route corrigée pour correspondre au nom appelé dans app.blade.php
+Route::get('/switch-mode/{mode}', [TaskController::class, 'switchMode'])->name('mode.switch');
 
 Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
 
-// Route Ajax appelée par Alpine.js
+// Route Ajax appelée par Alpine.js pour la suppression de projet
 Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->name('projects.destroy');
