0ab29695a7209c6714f49bde74fe225d5d2a025d
Bado239 <sallbado106@gmail.com>
2026-08-06 11:21:25 +0000

    Ajout de la route tasks.archive manquante

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ Route::get('/switch-mode/{mode}', [TaskController::class, 'switchMode'])->name('
 // Routes pour les tâches
 Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 
+// Route d'archivage (Ajoutée ici)
+Route::post('/tasks/{id}/archive', [TaskController::class, 'archive'])->name('tasks.archive');
+
 // Sécurité anti-erreur 405 : si une requête GET arrive sur /tasks, on redirige vers le dashboard
 Route::get('/tasks', function () {
     return redirect()->route('dashboard');
@@ -28,3 +31,6 @@ Route::delete('/projects/{project}', [ProjectController::class, 'destroy'])->nam
 
 // Si vous gérez les projets dans TaskController :
 Route::delete('/projects/{id}', [TaskController::class, 'destroyProject'])->name('projects.destroy');
+
+// Route pour la suppression d'étape/catégorie si gérée dans le contrôleur
+Route::delete('/categories/{id}', [TaskController::class, 'destroyCategory'])->name('categories.destroy');
