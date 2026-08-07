3eee403417ba88621c8ddbb253bd2f288e9af5ee
Bado239 <sallbado106@gmail.com>
2026-08-07 09:28:25 +0000

    Ajout des corrections

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -11,6 +11,13 @@ class Project extends Model
 
     protected $fillable = [
         'title',
+        'type',
+        'is_active',
+    ];
+
+    // Force Laravel à traiter is_active comme un vrai booléen pour PostgreSQL
+    protected $casts = [
+        'is_active' => 'boolean',
     ];
 
     public function tasks()
