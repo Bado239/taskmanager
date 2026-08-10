45e561d281c7bcabc790cdee68023765bf6e478d
Bado239 <sallbado106@gmail.com>
2026-08-10 08:40:45 +0000

    Nouvelle fonctionnalité

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -793,10 +793,6 @@
 
         </div>
     @endif
-            </div>
-
-        </div>
-    @endif
 </div>
 
 {{-- Formulaires de suppression dynamiques pour tous les projets et catégories fusionnés --}}
