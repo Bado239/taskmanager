995c96f9fb132582473503641dcbf7611b7bc5f7
Bado239 <sallbado106@gmail.com>
2026-08-31 15:35:48 +0000

    Correction recherche automatique cours

--- app/Http/Controllers/CourseResourceController.php
+++ app/Http/Controllers/CourseResourceController.php
@@ -3,10 +3,122 @@
 namespace App\Http\Controllers;
 
 use App\Models\CourseResource;
+use App\Services\CourseSearchService;
 use Illuminate\Http\Request;
 
 class CourseResourceController extends Controller
 {
+
+    protected CourseSearchService $searchService;
+
+
+    public function __construct(
+        CourseSearchService $searchService
+    )
+    {
+        $this->searchService = $searchService;
+    }
+
+
+
+    /**
+     * Recherche automatique des cours
+     */
+    public function searchCourses($taskId)
+    {
+
+        $task = \App\Models\Task::findOrFail($taskId);
+
+
+
+        /*
+        Matière recherchée
+        */
+        $subject = $task->subject
+            ?? $task->title
+            ?? '';
+
+
+
+        /*
+        Construction requête intelligente
+        */
+        $query = $subject . ' Master 1 cours PDF universitaire';
+
+
+
+        /*
+        Appel moteur DuckDuckGo
+        */
+        $results = $this->searchService
+            ->search($subject);
+
+
+
+        /*
+        Suppression anciens résultats
+        */
+        CourseResource::where(
+            'task_id',
+            $task->id
+        )->delete();
+
+
+
+        /*
+        Enregistrement nouveaux cours
+        */
+        foreach($results as $course)
+        {
+
+            CourseResource::create([
+
+                'task_id' => $task->id,
+
+                'title' =>
+                    $course['title'],
+
+                'url' =>
+                    $course['url'],
+
+                'source' =>
+                    $course['source'],
+
+                'type' =>
+                    $course['type'],
+
+                'file_type' =>
+                    $course['file_type'],
+
+                'is_university' =>
+                    $course['is_university'],
+
+                'score' =>
+                    $course['score'],
+
+                'saved' =>
+                    false,
+
+            ]);
+
+        }
+
+
+
+        return redirect()
+            ->route(
+                'tasks.learning',
+                $task->id
+            )
+            ->with(
+                'success',
+                'Recherche terminée'
+            );
+
+    }
+
+
+
 
     /**
      * Noter la pertinence d'un cours
@@ -33,6 +145,8 @@ class CourseResourceController extends Controller
 
 
 
+
+
     /**
      * Sauvegarder un cours
      */
@@ -55,6 +169,8 @@ class CourseResourceController extends Controller
         );
 
     }
+
+
 
 
 
