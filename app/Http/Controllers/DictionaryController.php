bf76e9078341a381dfce8738afba6a3a32ec4623
Bado239 <sallbado106@gmail.com>
2026-09-13 17:59:51 +0000

    Nouvelle fonctionnalité

--- app/Http/Controllers/DictionaryController.php
+++ app/Http/Controllers/DictionaryController.php
@@ -2,157 +2,55 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Support\Facades\Http;
+use App\Models\DictionaryWord;
 
 
 class DictionaryController extends Controller
 {
 
 
-    public function search($word)
-    {
+public function search($word)
+{
 
 
-        // Nettoyage du mot
+$word = strtolower(trim($word));
 
-        $word = strtolower(trim($word));
 
+$result = DictionaryWord::where(
+'word',
+$word
+)->first();
 
-        $word = preg_replace(
-            '/[^a-zàâçéèêëîïôùûüÿñæœ-]/u',
-            '',
-            $word
-        );
 
 
+if($result)
+{
 
-        if(empty($word))
-        {
-            return response()->json([
 
-                "word" => "",
+return response()->json([
 
-                "definition" => "Mot invalide."
+"word"=>$word,
 
-            ]);
-        }
+"definition"=>$result->definition
 
+]);
 
 
-        try {
+}
 
 
-            // Appel dictionnaire gratuit
 
-            $response = Http::timeout(30)
-                ->retry(2,1000)
-                ->get(
-                    "https://api.dictionaryapi.dev/api/v2/entries/fr/".$word
-                );
+return response()->json([
 
+"word"=>$word,
 
+"definition"=>"Mot non encore enregistré dans votre dictionnaire."
 
-            if($response->successful())
-            {
+]);
 
 
-                $data = $response->json();
+}
 
-
-
-                $definition =
-                $data[0]['meanings'][0]['definitions'][0]['definition']
-                ?? null;
-
-
-
-                if($definition)
-                {
-
-                    return response()->json([
-
-                        "word"=>$word,
-
-                        "definition"=>$definition
-
-                    ]);
-
-                }
-
-
-            }
-
-
-
-        } catch(\Exception $e) {
-
-
-            // Si API indisponible
-
-        }
-
-
-
-
-        /*
-        |
-        | Dictionnaire local de secours
-        |
-        */
-
-
-        $local = [
-
-
-            "moisson" =>
-            "Récolte des céréales arrivées à maturité.",
-
-
-            "moissonneur" =>
-            "Personne qui récolte les céréales.",
-
-
-            "faucille" =>
-            "Outil courbé utilisé pour couper les plantes.",
-
-
-            "prestesse" =>
-            "Rapidité et habileté dans l'action.",
-
-
-            "bienveillance" =>
-            "Disposition à vouloir le bien des autres.",
-
-
-            "serein" =>
-            "Calme et tranquille."
-
-
-        ];
-
-
-
-        return response()->json([
-
-
-            "word"=>$word,
-
-
-            "definition"=>
-
-            $local[$word]
-
-            ??
-
-            "Définition non disponible. Ce mot nécessite une explication contextuelle."
-
-
-
-        ]);
-
-
-
-    }
 
 
 }
