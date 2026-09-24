897427af7856da2c166aa901ba587546ff2213d7
Bado239 <sallbado106@gmail.com>
2026-09-24 18:04:50 +0000

    Nouvelle fonctionnalité

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1410,7 +1410,6 @@
                                             ->where('date',date('Y-m-d'))
                                             ->first();
                                 @endphp
-                                $goal->target_page
                             </td>
                         </tr>
 
